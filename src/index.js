6374b3511a674b9a3563e68a6a12e016a80a4c31
LauraDuane <redlunarcloud@gmail.com>
2024-04-04 21:13:44 -0400

    set up first way to input date and time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 /** @format */
 
-alert("Hello");
+let lasAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.qurySelector(".date");
+let losAngelesTimeElement = losAngelesElement.qurySelector(".time");
+let losAngelesTime = moment();
+losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do, YYYY");
+losAngelesTimeElement.innerHTML = `${losAngelesTime.format(
+  "h:mm:ss"
+)}<small>${losAngelesTime.format("A")}</small>`;
